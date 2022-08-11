b7a22e6e9fbacac4444145d776ee845595cc55c0
Babi Mumba <babimumba243@gmail.com>
2022-08-11 10:53:52 +0200

    add new file

--- app/src/main/java/com/media/retrive/MainActivity.kt
+++ app/src/main/java/com/media/retrive/MainActivity.kt
@@ -2,10 +2,25 @@ package com.media.retrive
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        save_btn.setOnClickListener {
+            var nom = firstname.text.toString()
+            var prenom = lastname.text.toString()
+
+            saveFirestore(nom,prenom )
+
+        }
+        
+
+    }
+
+    private fun saveFirestore(firstname: String, lastname: String?) {
+
     }
 }
