0b958bce6a7d219cf891ca7cef8057346119c326
Babi Mumba <babimumba243@gmail.com>
2022-08-11 11:15:54 +0200

    add enw file

--- app/src/main/java/com/media/retrive/MainActivity.kt
+++ app/src/main/java/com/media/retrive/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.media.retrive
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -8,9 +9,17 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var progressDialog: ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setMessage("loading in...")
+        progressDialog.setCancelable(false)
+        progressDialog.setCanceledOnTouchOutside(false)
 
         save_btn.setOnClickListener {
             var nom = firstname.text.toString()
