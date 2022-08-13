895554659829dcda605eb568549a8996ebfd6947
Babi Mumba <babimumba243@gmail.com>
2022-08-13 22:19:01 +0200

    add new file

--- app/src/main/java/com/media/retrive/ggl/GoogleAuth.kt
+++ app/src/main/java/com/media/retrive/ggl/GoogleAuth.kt
@@ -1,5 +1,6 @@
 package com.media.retrive.ggl
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.gms.auth.api.signin.GoogleSignIn
@@ -7,12 +8,13 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.media.retrive.R
+import kotlinx.android.synthetic.main.google_authentification.*
 
 class GoogleAuth : AppCompatActivity() {
-    private lateinit var mAut:FirebaseAuth
+    private lateinit var mAut: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
 
-    companion object{
+    companion object {
         private const val RC_SIGN_IN = 120
     }
 
@@ -25,12 +27,29 @@ class GoogleAuth : AppCompatActivity() {
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
-        googleSignInClient = GoogleSignIn.getClient(this,gso)
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
 
         mAut = FirebaseAuth.getInstance()
+        sign_in.setOnClickListener {
+            sign_in()
+        }
     }
-    private fun signinn(){
+
+    private fun sign_in() {
         val signiIntent = googleSignInClient.signInIntent
         startActivityForResult(signiIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val exception = task.exception
+            if (task.isSuccessful){
+
+            }
+        }
+
     }
 }
