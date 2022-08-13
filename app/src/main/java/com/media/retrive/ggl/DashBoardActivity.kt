50841759dc73ee71f57c75e7eebc37c005c203cb
Babi Mumba <babimumba243@gmail.com>
2022-08-13 23:23:40 +0200

    add new file

--- app/src/main/java/com/media/retrive/ggl/DashBoardActivity.kt
+++ app/src/main/java/com/media/retrive/ggl/DashBoardActivity.kt
@@ -1,13 +1,31 @@
 package com.media.retrive.ggl
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
 import com.media.retrive.R
+import kotlinx.android.synthetic.main.activity_dash.*
 
 class DashBoardActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dash)
+
+        mAuth = FirebaseAuth.getInstance()
+        val currentUser = mAuth.currentUser
+        id_user.text = currentUser?.uid
+        name_user.text = currentUser?.displayName
+        email_user.text = currentUser?.email
+
+        Glide.with(this)
+            .load(currentUser?.photoUrl)
+            .placeholder(R.drawable.idea)
+            .error(R.drawable.man)
+            .into(image_user)
+
 
     }
 }
