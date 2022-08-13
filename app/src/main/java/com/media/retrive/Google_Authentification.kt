3bdb50a84134958b0427d8b1273b170111fdb3b9
Babi Mumba <babimumba243@gmail.com>
2022-08-13 21:09:23 +0200

    add enw fiile

---
+++ app/src/main/java/com/media/retrive/Google_Authentification.kt
@@ -0,0 +1,11 @@
+package com.media.retrive
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Google_Authentification : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.google_authentification)
+    }
+}
