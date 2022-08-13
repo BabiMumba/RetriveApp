4018ec0cd4695e763fcde756fe0223f16d2925b1
Babi Mumba <babimumba243@gmail.com>
2022-08-13 21:52:34 +0200

    add new file

--- app/src/main/java/com/media/retrive/ggl/MainActivity.kt
+++ app/src/main/java/com/media/retrive/ggl/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.media.retrive.ggl
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
 import com.media.retrive.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mAuth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+        mAuth = FirebaseAuth.getInstance()
+        val user = mAuth.currentUser
+        Handler().postDelayed({
+
+            if (user != null){
+                val dashbordIntent = Intent(this, DashActivity::class.java)
+                startActivity(dashbordIntent)
+            }else{
+                val signinIntent = Intent(this,GoogleAuth::class.java)
+                startActivity(signinIntent)
+            }
+
+        })
+
     }
 }
