8e4c97e8cd9f0c193e0b5bece74134a0036f42d6
Babi Mumba <babimumba243@gmail.com>
2022-08-14 15:54:49 +0200

    add new file

---
+++ app/src/main/java/com/media/retrive/AdmodActivity.kt
@@ -0,0 +1,11 @@
+package com.media.retrive
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdmodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admod)
+    }
+}
