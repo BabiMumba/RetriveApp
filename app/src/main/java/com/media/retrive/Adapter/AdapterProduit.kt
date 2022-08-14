bbb56be2aa71dd31a4bae413138d9175c11af4ce
Babi Mumba <babimumba243@gmail.com>
2022-08-14 22:11:17 +0200

    add new file

--- app/src/main/java/com/media/retrive/Adapter/AdapterProduit.kt
+++ app/src/main/java/com/media/retrive/Adapter/AdapterProduit.kt
@@ -1,4 +1,21 @@
 package com.media.retrive.Adapter
 
+import android.view.View
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.media.retrive.R
+
 class AdapterProduit {
+    //viewHolder class for product .xml
+    inner  class HolderPrduct(itemview: View):RecyclerView.ViewHolder(itemview){
+
+        val iconIv:ImageView = itemview.findViewById(R.id.iconTv)
+        val titleTv:TextView = itemview.findViewById(R.id.txtTitletv)
+        val ratingbar:RatingBar = itemview.findViewById(R.id.etoile)
+        val description:TextView = itemview.findViewById(R.id.descripttion)
+
+
+    }
 }
