e63b8c0ccbf47ca4e9bf3ae77b2c4897e0eea82f
Babi Mumba <babimumba243@gmail.com>
2022-08-14 22:21:01 +0200

    add new file

--- app/src/main/java/com/media/retrive/Adapter/AdapterProduit.kt
+++ app/src/main/java/com/media/retrive/Adapter/AdapterProduit.kt
@@ -1,10 +1,12 @@
 package com.media.retrive.Adapter
 
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.ads.formats.MediaView
 import com.media.retrive.R
 
 class AdapterProduit {
@@ -18,8 +20,17 @@ class AdapterProduit {
 
     }
     inner class HoldenativeAds(itemview:View):RecyclerView.ViewHolder(itemview){
+
         val ad_appicon:ImageView = itemview.findViewById(R.id.ad_app_icon)
         val ad_headline:TextView = itemview.findViewById(R.id.ad_headline)
+        val ad_advertiser:TextView = itemview.findViewById(R.id.ad_advertiser)
+        val ad_stars:RatingBar = itemview.findViewById(R.id.ad_stars)
+        val ad_body:TextView = itemview.findViewById(R.id.ad_body)
+        val media_view:MediaView = itemview.findViewById(R.id.media_view)
+        val ad_price:TextView = itemview.findViewById(R.id.ad_price)
+        val ad_store:TextView = itemview.findViewById(R.id.ad_store)
+        val ad_action:Button = itemview.findViewById(R.id.ad_call_to_Action)
+
 
     }
 
